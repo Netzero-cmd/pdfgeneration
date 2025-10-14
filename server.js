43d44a66e130685cdf3fc3210c78678b2f6f7467
Netzero-cmd <Sanuj@54321>
2025-10-14 17:52:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('PDF Generation Service is running');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
